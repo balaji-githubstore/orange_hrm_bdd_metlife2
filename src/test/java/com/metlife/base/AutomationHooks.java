6f25bb6bd301d59b690697a1eab3ab11fcb074f4
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-08-24 16:59:28 +0530

    removed unwanted spaces

--- src/test/java/com/metlife/base/AutomationHooks.java
+++ src/test/java/com/metlife/base/AutomationHooks.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebDriver;
 public class AutomationHooks {
     public WebDriver driver;
 
-
     @After
     public void endScenario(Scenario scenario) {
 
@@ -22,8 +21,6 @@ public class AutomationHooks {
             byte[] bytes = ts.getScreenshotAs(OutputType.BYTES);
             scenario.attach(bytes, "image/png", scenario.getName());
         }
-
-
         if (driver != null) {
             driver.quit();
         }
