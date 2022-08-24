50f91e4a32156899762b73455814371c5fdb62ae
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-08-24 16:05:39 +0530

    bdd-framework - static driver and bdd concepts

--- src/test/java/com/metlife/runner/RunnerTest.java
+++ src/test/java/com/metlife/runner/RunnerTest.java
@@ -2,7 +2,7 @@ package com.metlife.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//with static driver
 @CucumberOptions(
         features = {"src/test/resources/feature"}
         ,glue = {"com.metlife.steps","com.metlife.base"}
