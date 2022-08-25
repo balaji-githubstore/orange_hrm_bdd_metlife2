8bcddc56c74dc2cfed1130f19a4df561dd6a90bb
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-08-25 13:58:47 +0530

    page objects updated

--- src/test/java/com/metlife/base/AutomationHooks.java
+++ src/test/java/com/metlife/base/AutomationHooks.java
@@ -6,7 +6,7 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
+//page objects
 public class AutomationHooks {
     public WebDriver driver;
 
