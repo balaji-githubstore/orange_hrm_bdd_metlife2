cc800d7e31824b23aa466a5ab70ee2652ea27037
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-08-25 13:34:49 +0530

    driver with non-static- dependency injection

--- src/test/java/com/metlife/steps/LoginSteps.java
+++ src/test/java/com/metlife/steps/LoginSteps.java
@@ -10,7 +10,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
-
+//no page objects
 import java.time.Duration;
 
 public class LoginSteps  {
